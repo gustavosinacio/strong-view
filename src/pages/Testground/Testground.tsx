a740324902b2a987db6de6348caab9fce3dfd0ba
Gustavo Inacio <gsinacio94@gmail.com>
2021-12-21 00:36:47 -0300

    removes puposed error

--- src/pages/Testground/Testground.tsx
+++ src/pages/Testground/Testground.tsx
@@ -2,12 +2,10 @@ import { useParams } from "react-router-dom";
 
 export const Testground = () => {
   const params = useParams();
-  const user = null;
 
   return (
     <div>
       <h2>Testground {params.groundId}</h2>
-      <h2>{user.name}</h2>
     </div>
   );
 };
